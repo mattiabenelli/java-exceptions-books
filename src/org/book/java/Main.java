47552db5ea7af9559af9f551750be2fd191ca788
mattiabenelli <benellimattia0@gmail.com>
2023-05-10 17:37:48 +0200

    es libri done

--- src/org/book/java/Main.java
+++ src/org/book/java/Main.java
@@ -2,14 +2,19 @@ package org.book.java;
 
 import java.util.Arrays;
 import java.util.Scanner;
+import java.io.File;
+import java.io.FileWriter;
 
 import org.book.java.obj.Libro;
 
 public class Main {
 	
+	private static final String FILENAME = "C:\\Users\\pasca\\Desktop\\libri_java.txt";
+	
 	public static void main(String[] args) {
 		
 		Scanner in = new Scanner (System.in);
+		
 		
 		System.out.println("Inserisci il numero di libri da aggiungere: ");
 		int arrLibriLng = in.nextInt();
@@ -30,15 +35,69 @@ public class Main {
 			System.out.println("Inserisci l'editore del libro: ");
 			String editore = in.next();
 			
-			arrLibri[i] = new Libro(titolo, numpagine, autore, editore);
-			System.out.println("libro inserito correttamente");
-			System.out.println("\n----------------------------------------\n");
-//			System.out.println(arrLibri[i]);
+			try {
+				
+				FileWriter myWriter = new FileWriter(FILENAME);
+				
+				arrLibri[i] = new Libro(titolo, numpagine, autore, editore);
+				
+				System.out.println("\nlibro inserito correttamente");
+				System.out.println("\n----------------------------------------\n");
+				
+				myWriter.close();
+				
+				
+			} catch(Exception e) {
+				
+				System.err.println("Errore nella creazione di Libro\n" + e.getMessage());
+				
+			}
 			
 		}
 		
+		try {
+			
+			FileWriter myWriter = new FileWriter(FILENAME);
+			
+			for(int x = 0; x < arrLibriLng; x++) {
+				
+				myWriter.write(arrLibri[x].toString() + "\n");
+			}
+			
+			myWriter.close();
+			
+			
+		} catch(Exception e) {
+			
+			System.err.println("Errore nella scrittura di Libro\n" + e.getMessage());
+			
+		}
+//		System.out.println(arrLibri[i]);
+		
+		try {
+			
+			File mioFile = new File(FILENAME);
+			Scanner reader = new Scanner(mioFile);
+			
+			while (reader.hasNextLine()) {
+		        String data = reader.nextLine();
+		        System.out.println(data);
+		      }
+			
+			
+		} catch (Exception e) {
+			
+			System.err.println("Errore nella lettura di Libro\n" + e.getMessage());
+		}
+		
 		in.close();
-		System.out.println(Arrays.asList(arrLibri));
+//		System.out.println(Arrays.asList(arrLibri) + "\n");
+
+		
+		
+		
 	}
+	
+	
 
 }
--- src/org/book/java/obj/Libro.java
+++ src/org/book/java/obj/Libro.java
@@ -7,7 +7,7 @@ public class Libro {
 	private String autore;
 	private String editore;
 	
-	public Libro(String titolo, int numpagine, String autore, String editore) {
+	public Libro(String titolo, int numpagine, String autore, String editore) throws Exception {
 		
 		setTitolo(titolo);
 		setNumpagine(numpagine);
@@ -19,7 +19,13 @@ public class Libro {
 		return titolo;
 	}
 	
-	public void setTitolo(String titolo) {
+	public void setTitolo(String titolo) throws Exception {
+		
+		if(titolo.length() == 0) {
+			
+			throw new Exception("Il titolo non può essere vuoto");
+		}
+		
 		this.titolo = titolo;
 	}
 	
@@ -27,15 +33,27 @@ public class Libro {
 		return numpagine;
 	}
 	
-	public void setNumpagine(int nPagine) {
-		this.numpagine = nPagine;
+	public void setNumpagine(int numpagine) throws Exception {
+		
+		if (numpagine <= 0) {
+			
+			throw new Exception("Il numero di pagine non può essere minore o pari a zero");
+		}
+		
+		this.numpagine = numpagine;
 	}
 	
 	public String getAutore() {
 		return autore;
 	}
 	
-	public void setAutore(String autore) {
+	public void setAutore(String autore) throws Exception {
+		
+		if(autore.length() == 0) {
+			
+			throw new Exception("L'autore non può essere vuoto");
+		}
+		
 		this.autore = autore;
 	}
 	
@@ -53,7 +71,7 @@ public class Libro {
 		return "titolo: " + getTitolo() +
 				"\nnumero di pagine: " + getNumpagine() +
 				"\nautore del libro: " + getAutore() +
-				"\neditore del libro: " + getEditore();
+				"\neditore del libro: " + getEditore() + "\n";
 	}
 	
 	
